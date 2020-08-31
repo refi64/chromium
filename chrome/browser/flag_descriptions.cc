7db034e2e7b0f18776e8556eb3d46a7b61ca06ec
Ryan Gonzalez <rymg19@gmail.com>
2020-08-31 15:26:52 -0500

    XDG file chooser portal support

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -3376,6 +3376,17 @@ const char kSyncSetupFriendlySettingsDescription[] =
 
 #endif  // !defined(OS_ANDROID)
 
+// Linux -----------------------------------------------------------------------
+
+#if defined(OS_LINUX)
+
+const char kXdgFileChooserPortalName[] = "Use the XDG file chooser portal";
+const char kXdgFileChooserPortalDescription[] =
+    "File selection will use the XDG portal rather than GTK/KDE-specific "
+    "dialogs";
+
+#endif
+
 // Windows ---------------------------------------------------------------------
 
 #if defined(OS_WIN)
