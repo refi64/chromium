97e31a50c6cc6d1859708a531db283e0314b43c3
Ryan Gonzalez <rymg19@gmail.com>
2020-11-11 17:13:29 -0600

    fixup: desktop shortcuts

--- chrome/browser/ui/views/create_application_shortcut_view.h
+++ chrome/browser/ui/views/create_application_shortcut_view.h
@@ -71,7 +71,6 @@ class CreateChromeApplicationShortcutView : public views::DialogDelegateView,
   base::Callback<void(bool)> close_callback_;
 
   // May be null if the platform doesn't support a particular location.
-  views::Checkbox* desktop_check_box_ = nullptr;
   views::Checkbox* menu_check_box_ = nullptr;
   views::Checkbox* quick_launch_check_box_ = nullptr;
 
