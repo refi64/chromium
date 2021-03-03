endlessm/chromium-browser

x11: Set _NET_WM_BYPASS_COMPOSITOR for fullscreen
This improves performance at full screen.

ui/platform_window/x11/x11_window.cc
@@ -258,6 +258,10 @@ void X11Window::ToggleFullscreen() {
   state_ = new_state;
   SetFullscreen(fullscreen);
 
+  unsigned long value = fullscreen;
+
+  SetBypassCompositor(value);
+
   if (unmaximize_and_remaximize)
     Maximize();
 

refi64/chromium

ui/platform_window/x11/x11_window.cc
@@ -300,6 +300,8 @@ void X11Window::ToggleFullscreen() {
   state_ = new_state;
   SetFullscreen(fullscreen);
 
+  SetBypassCompositor(fullscreen);
+
   if (unmaximize_and_remaximize)
     Maximize();
 